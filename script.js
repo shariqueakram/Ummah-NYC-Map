02233f0d7473649b9f905891c75524e3904ca507
sharique.akram <shariqueakram@gmail.com>
2024-07-24 22:26:19 -0400

    Updated mainframe to resolve Google API issues not working w/ VSCode extension

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-function initiMap(){ //Loads Google Maps at a specific point on the map
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8,
-        mapId: '75851ff91bc5e98a'
-        });
-}
+// Placeholder for JavaScript logic if you need to fetch data from a server, etc.
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
+});
